008ec8f56544930fba5ce9220817d43600540758
KshitijBhat <me200003042@iiti.ac.in>
2022-07-03 00:34:13 +0530

    IA done

--- Intersections.cpp
+++ Intersections.cpp
@@ -21,7 +21,7 @@ Point_2 polarToCartesian(double r, double phi, Point_2 o);
 double * CartesianToPolar(Point_2 p, Point_2 o);
 bool sortcol(const vector<double>& v1, const vector<double>& v2);
 bool sortcol2(const vector<double>& v1, const vector<double>& v2);
-void cartesianToPolarArr(double points[28][2],int num_pts, Point_2 o);
+double * cartesianToPolarArr(double points[28][2],int num_pts, Point_2 o);
 bool pointIn(Interval I, Point_2 p);
 void print(Point_2 p);
 void print(Circular_arc_point_2 p);
@@ -50,6 +50,8 @@ void print(Line_arc_2 seg){
     print(seg.target());
     cout<<endl;
 }
+
+
 
 void print(double pts[5]){
     cout<<"Points: (";
@@ -215,6 +217,14 @@ double * normalcy( Point_2 p, Circular_arc_2 ar)
 }
 
 
+typedef struct IntervalStruct{
+    double radius1 = 69;
+    double radius2 = 69;
+    double theta1 = 69;
+    double theta2 = 69;
+    double origin1 = 69;
+    double origin2 = 69;
+}IntervalStruct;
 
 class Interval{
     
@@ -234,7 +244,8 @@ class Interval{
     Point_2 r1t1, r2t1, r1t2, r2t2;
     
     friend Point_2 polarToCartesian(double r, double phi, Point_2 o);
-    friend void cartesianToPolarArr(double points[28][2],int num_pts, Point_2 o);
+    friend double * cartesianToPolarArr(double points[28][2],int num_pts, Point_2 o);
+    // friend struct IntervalStruct;
     
     Interval( double r1, double r2,  double t1, double t2,  Point_2 o)
     {
@@ -254,23 +265,26 @@ class Interval{
         arc1 = Circular_arc_2(Circle_2(o,r1*r1),Circular_arc_point_2(r1t1),Circular_arc_point_2(r1t2));
         arc2 = Circular_arc_2(Circle_2(o,r2*r2),Circular_arc_point_2(r2t1),Circular_arc_point_2(r2t2));  
     }
-    Interval( double r1, double r2,  double t1, double t2,  double originx, double originy)
+
+
+    Interval(struct IntervalStruct IS)
     {
-        Point_2 o = Point_2(originx,originy);
-        radius1 = r1; radius2 = r2;
-        theta1 = t1; theta2 = t2;
+
+        Point_2 o = Point_2(IS.origin1,IS.origin2);
+        radius1 = IS.radius1; radius2 = IS.radius2;
+        theta1 = IS.theta1; theta2 = IS.theta2;
         origin = o;
 
-        r1t1 = polarToCartesian(r1,t1,o);
-        r2t1 = polarToCartesian(r2,t1,o);
-        r1t2 = polarToCartesian(r1,t2,o);
-        r2t2 = polarToCartesian(r2,t2,o);
+        r1t1 = polarToCartesian(IS.radius1,IS.theta1,o);
+        r2t1 = polarToCartesian(IS.radius2,IS.theta1,o);
+        r1t2 = polarToCartesian(IS.radius1,IS.theta2,o);
+        r2t2 = polarToCartesian(IS.radius2,IS.theta2,o);
 
 
         seg1 = Line_arc_2(r1t1,r2t1);
         seg2 = Line_arc_2(r1t2,r2t2);
-        arc1 = Circular_arc_2(Circle_2(o,r1*r1),Circular_arc_point_2(r1t1),Circular_arc_point_2(r1t2));
-        arc2 = Circular_arc_2(Circle_2(o,r2*r2),Circular_arc_point_2(r2t1),Circular_arc_point_2(r2t2));  
+        arc1 = Circular_arc_2(Circle_2(o,IS.radius1*IS.radius1),Circular_arc_point_2(r1t1),Circular_arc_point_2(r1t2));
+        arc2 = Circular_arc_2(Circle_2(o,IS.radius2*IS.radius2),Circular_arc_point_2(r2t1),Circular_arc_point_2(r2t2));  
     }
 
     // void IntervalFromEndpoints(Point_2 r1t1, Point_2 r2t1, Point_2 r1t2, Point_2 r2t2){
@@ -288,7 +302,9 @@ class Interval{
 
 };
 
-void IntervalAnalysis(Interval I,Interval Iprime){
+
+
+IntervalStruct IntervalAnalysis(Interval I,Interval Iprime){
     Circular_arc_2 arcs[2] = {I.arc1,I.arc2};
     Circular_arc_2 arcprimes[2] = {Iprime.arc1,Iprime.arc2};
     Line_arc_2 segs[2] = {I.seg1,I.seg2};
@@ -393,40 +409,69 @@ void IntervalAnalysis(Interval I,Interval Iprime){
     normal = normalcy(I.origin,arcprimes[1]);
     
     if(normal[4]== 1.0 ){
-        if (sqrt(normal[0]*normal[0]+normal[1]*normal[1])<I.radius2 && sqrt(normal[0]*normal[0]+normal[1]*normal[1])>I.radius1){
+        if (pointIn(I,Point_2(normal[0],normal[1]))){
             points[num_pts][0] = normal[0];
             points[num_pts][1] = normal[1];
             num_pts += 1;
         }    
     }
 
-    // double *tangents;
-    // tangents = tangency(I.origin,arcprimes[1]);
-    // // todo: add condition to ensure tangency lies in I
-    // if (tangents[4]== 2.0){
-
-    //     points[num_pts][0] = tangents[0];
-    //     points[num_pts][1] = tangents[1];
-    //     points[num_pts+1][0] = tangents[2];
-    //     points[num_pts+1][1] = tangents[3];
-    //     num_pts += 2;
-    // }
-    // else if(tangents[4]== 1.0){
-    //     points[num_pts][0] = tangents[0];
-    //     points[num_pts][1] = tangents[1];
-    //     num_pts += 1;
-    // }
-
-
-    for(int ii=0;ii<num_pts;ii++){
-        cout<<"("<<points[ii][0]<<","<<points[ii][1]<<")"<<endl;
-
+    double *tangents;
+    tangents = tangency(I.origin,arcprimes[1]);
+    // Added condition to ensure tangency lies in I
+    if (tangents[4]== 2.0){
+        if(pointIn(I,Point_2(tangents[0],tangents[1]))){
+            points[num_pts][0] = tangents[0];
+            points[num_pts][1] = tangents[1];
+            num_pts += 1;
+        }
+        if(pointIn(I,Point_2(tangents[2],tangents[3]))){
+            points[num_pts+1][0] = tangents[2];
+            points[num_pts+1][1] = tangents[3];
+            num_pts += 1;
+        }
+        
     }
-    cout<<"Num_pts: "<<num_pts<<endl;
+    else if(tangents[4]== 1.0){
+        if(pointIn(I,Point_2(tangents[0],tangents[1]))){
+            points[num_pts][0] = tangents[0];
+            points[num_pts][1] = tangents[1];
+            num_pts += 1;
+        }
+    }
+
+
+    // for(int ii=0;ii<num_pts;ii++){
+    //     cout<<"("<<points[ii][0]<<","<<points[ii][1]<<")"<<endl;
+
+    // }
+    // cout<<"Num_pts: "<<num_pts<<endl;
     // for (int i=0;i<num_pts;i++){
     //     cout<<*points[i]<<*(points[i]+1)<<endl;
     // }
-    cartesianToPolarArr(points,num_pts,I.origin);
+    double x0,y0;
+    x0 = to_double(I.origin.x());y0 = to_double(I.origin.y());
+    vector<vector<double>> cartez;
+    for (int i=0;i<num_pts;i++){
+        double x = *points[i]; double y = *(points[i]+1);
+        cartez.push_back({sqrt((x-x0) * (x-x0) + (y-y0) * (y-y0)), atan2(y-y0 , x-x0)});
+    }
+    sort(cartez.begin(), cartez.end(), sortcol);
+    vector<vector<double>> cartezt = cartez;
+    sort(cartez.begin(), cartez.end(), sortcol2);
+
+    double theta1 = cartezt[0][1]; double theta2 = cartezt[num_pts-1][1];
+    double radius1 = cartez[0][0]; double radius2 = cartez[num_pts-1][0];
+    
+    // double * J = new double[6]{radius1,radius2,theta1,theta2,x0,y0};
+    IntervalStruct J;
+    J.radius1 = radius1;
+    J.radius2 = radius2;
+    J.theta1 = theta1;
+    J.theta2 = theta2;
+    J.origin1 = x0;
+    J.origin2 = y0;
+    return J;
 }
 
 bool sortcol(const vector<double>& v1, const vector<double>& v2)
@@ -445,7 +490,7 @@ double * CartesianToPolar(Point_2 p, Point_2 o){
     return pt;     
 }
 
-void cartesianToPolarArr(double points[28][2],int num_pts,Point_2 o) {
+double * cartesianToPolarArr(double points[28][2],int num_pts,Point_2 o) {
     double x0,y0;
     x0 = to_double(o.x());y0 = to_double(o.y());
     vector<vector<double>> cartez;
@@ -455,16 +500,14 @@ void cartesianToPolarArr(double points[28][2],int num_pts,Point_2 o) {
     }
     sort(cartez.begin(), cartez.end(), sortcol);
     vector<vector<double>> cartezt = cartez;
-    for (int i=0;i<num_pts;i++){
-        cout<<cartez[i][0]<<", " <<cartez[i][1]<<endl;
-    }
+    
     sort(cartez.begin(), cartez.end(), sortcol2);
 
     double theta1 = cartezt[0][1]; double theta2 = cartezt[num_pts-1][1];
     double radius1 = cartez[0][0]; double radius2 = cartezt[num_pts-1][0];
-    cout<<"Interval"<<theta1<<", "<<theta2<<", "<<radius1<<", "<<radius2<<endl;
-    
-        
+
+    double * I = new double[6]{radius1,radius2,theta1,theta2,x0,y0};
+    return I;
 }
 
 Point_2 polarToCartesian(double r, double phi, Point_2 o) {
@@ -509,18 +552,20 @@ int main() {
     Interval I1 = Interval(1,2,0.5,1.7,Point_2(0,0));
     // print(I1.arc2);
     // print(I2.arc2);
-    IntervalAnalysis(I1,I2);
+    cout<<IntervalAnalysis(I1,I2).radius1;
+    // print(IntervalAnalysis(I1,I2).arc1);
+    
     // print(arc_arc(I1.arc2,I2.arc2));
-    
-    
     
     // print(polarToCartesian(5,1.57096));
 }
+
+
 
 extern "C" {
     // Geek* Geek_new(){ return new Geek(); }
     // void interval_analysis(double I1r1,double I1r2, double I1t1, double I1t2, double I1ox,double I1oy,double I2r1,double I2r2, double I2t1, double I2t2, double I2ox,double I2oy)
     // {IntervalAnalysis(I1r1,I1r2,I1t1,I1t2,I1ox,I1oy,I2r1,I2r2,I2t1,I2t2,I2ox,I2oy);}
-    Interval* Interval_new(double r1, double r2,  double t1, double t2,  double originx, double originy){ return new Interval(r1, r2,  t1, t2,  originx, originy); }
-    void interval_analysis(Interval I1,Interval I2){ IntervalAnalysis(I1,I2); }
+    Interval* Interval_new(double r1, double r2,  double t1, double t2,  double originx, double originy){ return new Interval(r1, r2,  t1, t2,  Point_2(originx, originy)); }
+    IntervalStruct* interval_analysis(IntervalStruct I1,IntervalStruct I2){return new IntervalStruct(IntervalAnalysis(Interval(I1),Interval(I2)));}
 }
--- IntervalAnalysisPy.py
+++ IntervalAnalysisPy.py
@@ -1,9 +1,14 @@
 # import the module
 import ctypes
+import _ctypes
 import numpy as np
 from matplotlib.patches import Arc as Arc_patch
 import matplotlib.pyplot as plt
 from matplotlib import collections  as mc
+from numpy.ctypeslib import ndpointer
+import time
+
+
 # compiling instructions
 # g++ -c -fPIC Intersections.cpp -o Intersections.o -std=c++11 -lmpfr -lgmp -lCGAL
 # g++ -shared -Wl,-soname,libIntersections.so -o libIntersections.so Intersections.o -std=c++11 -lmpfr -lgmp -lCGAL
@@ -16,6 +21,15 @@ lib = ctypes.cdll.LoadLibrary('./libIntersections.so')
 # function calling
 # lib.interval_analysis(1.00,2.00,0.500,1.700,  0.00,0.00,   
 #                       1.00,2.00,2.00,3.5,     3.00,2.00)
+
+class interval_struct(ctypes.Structure):
+    _fields_ = [('radius1', ctypes.c_double),
+                ('radius2', ctypes.c_double),
+                ('theta1', ctypes.c_double),
+                ('theta2', ctypes.c_double),
+                ('origin1', ctypes.c_double),
+                ('origin2', ctypes.c_double)]
+          
 
 # create a Geek class
 class Interval:
@@ -31,14 +45,27 @@ class Interval:
         self.theta1 = t1
         self.theta2 = t2
         self.origin = pt
+        self.IS = interval_struct()
+        self.IS.radius1 = ctypes.c_double(r1)
+        self.IS.radius2 = ctypes.c_double(r2)
+        self.IS.theta1 = ctypes.c_double(t1)
+        self.IS.theta2 = ctypes.c_double(t2)
+        self.IS.origin1 = ctypes.c_double(pt[0])
+        self.IS.origin2 = ctypes.c_double(pt[1])
 
-
-
-
+    def from_struct(self,IS):
+        return Interval(IS.radius1,IS.radius2,IS.theta1,IS.theta2,np.array([IS.origin1,IS.origin2]))
   
     # define method
     def interval_analysis(self,I2):
-        lib.interval_analysis(self.obj,I2.obj)
+        foo = lib.interval_analysis
+        foo.argtypes = [interval_struct,interval_struct]
+        foo.restype = ctypes.c_void_p
+        #interval_struct #ctypes.c_void_p #ctypes.POINTER(ctypes.c_double)
+        # print(foo(self.IS,I2.IS).radius1)
+        result = interval_struct.from_address(foo(self.IS,I2.IS))
+        return self.from_struct(result)
+
 
     def transform_interval(self,I,theta):
         R = np.array([[np.cos(theta),np.sin(theta)],[-np.sin(theta),np.cos(theta)]])
@@ -66,24 +93,42 @@ class Interval:
 
 
 # create a Geek class object
-I2 = Interval(1,2,2,3.5,np.array([5+3,5+2]))
-I1 = Interval(1,2,0.5,1.7,np.array([5+0,5+0]))
+# I2 = Interval(1,2,2,3.5,np.array([5+3,5+2]))
+# I1 = Interval(1,2,0.5,1.7,np.array([5+0,5+0]))
 
+# I2 = Interval(1,2,3,4.5,np.array([2,3]))
+# I1 = Interval(1,2,0.5,1.7,np.array([0,0]))
+
+# I2 = Interval(1,2,2,3.5,np.array([1,0]))
+# I1 = Interval(1,2,0.5,1.7,np.array([0,0]))
+I2 = Interval(1,2,-0.5,1.5,np.array([-3.5,0]))
+I1 = Interval(1,2,1.57,3.15,np.array([0,0]))
 # object method calling
-I1.interval_analysis(I2)
-
+# print("hello: ",I1.interval_analysis(I2))
+st_time = time.time()
+J = I1.interval_analysis(I2)
+print("Time: ", time.time()-st_time)
 fig= plt.figure(figsize = (16,8))
 ax = fig.add_subplot(1,2,1)
 I2.draw(ax,'red')
 I1.draw(ax,'blue')
-
+J.draw(ax,'green')
 # I1.draw(ax,'blue')
 plt.axis('equal')
 plt.grid()
 ax1 = fig.add_subplot(1,2,2)
 # I2.draw(ax1,'red')
-I1.rotate_interval(1.5708).draw(ax1,'blue')
-I1.transform_interval(I2,1.5708).draw(ax1,'red')
+s_t = time.time()
+II1 = I1.rotate_interval(1.5708)
+
+II2 = I1.transform_interval(I2,1.5708)
+t_t = time.time()
+JJ = II1.interval_analysis(II2)
+print(f"Python: {t_t-s_t} C++: {time.time()-t_t}")
+
+II1.draw(ax1,'blue')
+II2.draw(ax1,'red')
+JJ.draw(ax1,'green')
 plt.axis('equal')
 plt.grid()
 plt.show()
