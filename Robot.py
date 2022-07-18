c772026c1e7f5e3e0afbaed1758044f3c9fd7cb1
KshitijBhat <me200003042@iiti.ac.in>
2022-07-18 15:43:09 +0530

    simulate random walks

--- Robot.py
+++ Robot.py
@@ -4,12 +4,14 @@ from Interval import *
 import cv2
 import json
 
+np.random.seed(3)
+
 class Robot:
     radius = 15
     bot_color = 'b'
     color = 'blue'
     Δt = 1
-    dt = 0.01
+    dt = 0.1
     obstacles = None
     max_v = 30
     max_w = 1.5
@@ -38,15 +40,14 @@ class Robot:
 
     @property
     def interval_hull(self):
-        r =  2
-        theta1 = self.yaw - (self.max_w/2)*self.Δt*1.2
-        theta2 = self.yaw + (self.max_w/2)*self.Δt*1.2
-        origin = np.array([self.x_bot+self.radius*r*(np.cos(theta1)+np.cos(theta2))/(1e-6 + np.sin(theta1-theta2)),
-                       self.y_bot+self.radius*r*(np.sin(theta1)+np.sin(theta2))/(1e-6 + np.sin(theta1-theta2))])
+        theta1 = self.yaw - (self.max_w/2)*self.Δt
+        theta2 = self.yaw + (self.max_w/2)*self.Δt
+        origin = np.array([self.x_bot+self.radius*(np.cos(theta1)+np.cos(theta2))/(1e-6 + np.sin(theta1-theta2)),
+                       self.y_bot+self.radius*(np.sin(theta1)+np.sin(theta2))/(1e-6 + np.sin(theta1-theta2))])
 
-        n = self.radius*r/np.sin(1e-6+(theta2-theta1)/2)
-        nu1 = n - self.radius*r
-        nu2 = n + self.radius*r + (self.max_v)*self.Δt
+        n = self.radius/np.sin(1e-6+(theta2-theta1)/2)
+        nu1 = n - self.radius
+        nu2 = n + self.radius + (self.max_v)*self.Δt
         return Interval(nu1,nu2,theta1,theta2,origin)
     
     def collision_free_command(self,ax=None):
@@ -106,8 +107,23 @@ class Robot:
         else:
             self.yaw = yaw[-1]
 
-        return x,y,yaw
+        # return x,y,yaw
+
+
+    def move2(self,v,w,ax=None):
+        yaw = self.yaw + w * self.dt
+        x = self.x_bot + v * np.cos(yaw) * self.dt
+        y = self.y_bot + v * np.sin(yaw) * self.dt
         
+        if (x<=500 and x>=20 and y<=500 and y>=20):
+            self.yaw = yaw
+            self.x_bot = x
+            self.y_bot = y
+        else:
+            self.yaw = yaw
+
+        # return x,y,yaw
+
     def draw(self,ax):
         bot_circle = plt.Circle( (self.x_bot, self.y_bot),self.radius,color=self.bot_color)
         arrow = ax.arrow(self.x_bot,self.y_bot,self.radius*np.cos(self.yaw),self.radius*np.sin(self.yaw))
@@ -121,22 +137,33 @@ class Robot:
 
 
 if __name__ == "__main__":
-    # # robot = Robot(4,4,0,100,1)
+    # robot = Robot(4,4,0,100,1)
     # robot = Robot(4,4,3.14,100,1)
-    # # obstacle = Interval(0,40,1,2.5,[50,-40])
+    # obstacle = Interval(0,40,1,2.5,[50,-40])
     # obstacle = Interval(0,40,-1,1,[-40,40])
+    # robot1 = Robot(-50,-40,0,30,1)
+    # robot1.bot_color = 'red'
     # v = robot.max_v*np.ones(robot.num_samples)
     # w = np.array([(2*i*robot.max_w/(robot.num_samples-1)-robot.max_w) for i in range(robot.num_samples)])
     # fig,ax = plt.subplots(figsize=(8,8))
     # ax. set_aspect('equal')
-    # # robot.draw(ax)
+    # robot.draw(ax)
+    # robot.draw(ax)
+    # robot.interval_hull.draw(ax,color='b')
     # robot.interval.draw(ax,color='b')
+
+    # robot1.draw(ax)
+    # robot1.interval.draw(ax,color='r')
+    # robot1.interval_hull.draw(ax,color='r')
     # obstacle.draw(ax,'red')
-    # J = robot.interval.interval_analysis(obstacle)
+    # J = robot.interval.interval_analysis(robot1.interval_hull)
     # J.draw(ax,"green")
-    # # changes w's in J
-    # w1 = (J.theta1-0)/robot.Δt
-    # w2 = (J.theta2-0)/robot.Δt
+    # changes w's in J
+
+    # # something SUS about this
+    # w1 = (J.theta1-robot.yaw)/robot.Δt
+    # w2 = (J.theta2-robot.yaw)/robot.Δt
+
     # vj = J.radius1/robot.Δt
     # for i in range(robot.num_samples):
     #     if w[i]>=2*w1 and w[i]<=2*w2:
@@ -163,6 +190,7 @@ if __name__ == "__main__":
     #robot = Robot(900,800,4.2,100,0,1,0)
     #robot = Robot(800,770,4.5,80,0,1,0)
 
+
     robot1 = Robot(200,200,-np.pi/2,35,1.13)
     robot2 = Robot(100,200, 0,35,1.13)
     robot3 = Robot(250,200, 0.5,35,1.13)
@@ -175,16 +203,16 @@ if __name__ == "__main__":
     # robot.set_environment(obstacles)
     
     fig,ax = plt.subplots(figsize=(8,8))
-    
-    for i in range(500):
+
+    for i in range(5000):
         plt.cla()
-   
+
         ax. set_aspect('equal')
         ax.set_xlim([0, 524])
         ax.set_ylim([0, 524])
         # ax.imshow(arena,cmap = plt.cm.gray_r,origin = 'lower')
         # for obstacle in obstacles:
-        #     obstacle.draw(ax,'red')
+            # obstacle.draw(ax,'red')
         robot1.draw(ax)
         robot1.interval_hull.draw(ax,'blue')
         robot1.interval.draw(ax,'blue')
@@ -198,25 +226,26 @@ if __name__ == "__main__":
         robot4.interval_hull.draw(ax,'black')
         robot4.interval.draw(ax,'black')
         
+        if i%(robot1.Δt//robot1.dt) == 0:
+            v1,w1 = robot1.collision_free_command(ax)
+            
 
-        v1,w1 = robot1.collision_free_command(ax)
+            v2,w2 = robot2.collision_free_command(ax)
+
+            v3,w3 = robot3.collision_free_command(ax)
+
+            v4,w4 = robot4.collision_free_command(ax)
+            plt.savefig(f'captures/fig{i}.png',bbox_inches='tight',dpi=133)
+
+        robot1.move2(v1,w1)
+        robot2.move2(v2,w2)
+        robot3.move2(v3,w3)
+        robot4.move2(v4,w4)
+    
+        # plt.axis("off")
+        # plt.savefig(f'captures/fig{i}.png',bbox_inches='tight',dpi=133)
+        # plt.show()
+        plt.pause(0.000000001)
+    
+    
         
-
-        v2,w2 = robot2.collision_free_command(ax)
-
-        v3,w3 = robot3.collision_free_command(ax)
-
-        v4,w4 = robot4.collision_free_command(ax)
-
-        robot1.move(v1,w1)
-        robot2.move(v2,w2)
-        robot3.move(v3,w3)
-        robot4.move(v4,w4)
-        
-        plt.axis("off")
-        plt.savefig(f'captures/fig{i}.png',bbox_inches='tight',dpi=133)
-
-        plt.pause(0.0001)
-    
-    
-    
